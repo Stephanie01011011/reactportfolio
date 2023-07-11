cf83d2e1299b82cd44dc99a02136b30a907dfcdb
Stephanie Livengood <sreneelive@gmail.com>
2023-07-11 17:22:12 -0400

    Added new components -nav and projects page, added styling.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { Link } from "react-router-dom";
-
+import Nav from "./Nav";
+import './app.css';
 function App() {
   return (
-    <div>
+    <>
       <Link to="/about">About</Link>
-     <h2>Github Pages</h2>
-     <h3>Deploying React to Github</h3>
-    </div>
+      
+ <Nav />
+ <div className="Hero">
+ <div className="heroText">
+  <h1>Welcome to my portfolio</h1>
+  <p>Web developer with internship experience, a positive attitude, and plenty of projects to demonstrate skills. Please feel free to reach out to set up a call or just ask any questions you may have!</p>
+  <button>Email Me!</button>
+  </div>
+ <div className="stephimg"></div>
+ </div>
+ </>
+    
   );
 }
 
---
+++ src/Project.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+const Project = () => {
+    return ( 
+        <div>
+          <Link to="/">Home</Link>
+          <h2>Projects Page</h2> 
+        </div>
+        );
+}
+export default Project;
