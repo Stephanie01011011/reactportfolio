f4e8937821137950451aa22089fd850f2d1005e5
Stephanie Livengood <sreneelive@gmail.com>
2023-07-11 17:07:23 -0400

    Pages not working. Made update to render function

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { HashRouter, Routes, Route } from "react-router-dom";
 import About from './About';
 
-ReactDOM.render(
+const RootComponent = () => (
   <React.StrictMode>
     <HashRouter>
       <Routes>
@@ -12,9 +12,10 @@ ReactDOM.render(
         <Route path="/about" element={<About/>}/>
       </Routes>
     </HashRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  </React.StrictMode>
+)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RootComponent />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
